529ed83286433b62fa64dce8b142062bd2ab4668
dlsrhdwlsmd22 <dlsrhdwlsmd22@gmail.com>
2019-08-16 10:50:13 +0900

    react vol.1

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 class Board extends Component{
-
+    //생명주기 발생시 STS의 RESTful에 접근해서 정보를 가져옴.
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8080/web/boardc.json?no=5')
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(function(error){
+            console.log('에러가 발생함'+error);
+        })
+    }
     render(){
         return(
             <div>
